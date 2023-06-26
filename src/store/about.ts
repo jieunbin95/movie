70b596d9378e01364fb5e8f1fa382ed492a05582
jieunbin95 <bje0416@naver.com>
2023-06-26 15:51:51 +0900

    Compiled Typescript

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,14 @@
 import { Store } from "../core/jieun";
 
-export default new Store({
+interface State{
+  photo:string
+  name:string
+  email:string
+  blog:string
+  github:string
+  repository:string
+}
+export default new Store<State>({
   photo:'https://heropy.blog/css/images/logo.png',
   name:'JIEUN/JIEUN BIN',
   email:'bje0416@naver.com',
